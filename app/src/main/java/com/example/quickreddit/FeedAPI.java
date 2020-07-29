f024e8ddc64c71c2378dda2d8e525c15c36def8f
Ryan Corey <rcorey@pda.local>
2020-07-29 14:46:11 -0400

    Initial commit

---
+++ app/src/main/java/com/example/quickreddit/FeedAPI.java
@@ -0,0 +1,15 @@
+package com.example.quickreddit;
+
+
+import com.example.quickreddit.model.Feed;
+import retrofit2.http.GET;
+import retrofit2.Call;
+
+public interface FeedAPI {
+
+    String BASE_URL = "https://www.reddit.com/r/";
+
+    @GET("metalcore.rss")
+    Call<Feed> getFeed();
+
+}
