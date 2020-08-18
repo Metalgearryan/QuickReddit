3a363c2d9838e221a129596e3fcd5d6644c0df02
rcorey <metalgearryan@gmail.com>
2020-08-18 13:36:01 -0400

    Add CommentsActivity class to be able to load user comments

---
+++ app/src/main/java/com/example/quickreddit/CommentsActivity.java
@@ -0,0 +1,122 @@
+package com.example.quickreddit;
+
+import android.app.AppComponentFactory;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+public class CommentsActivity extends AppCompatActivity {
+
+    private static final String TAG = "CommentsActivity";
+
+    private static String postURL;
+    private static String postThumbnailURL;
+    private static String postTitle;
+    private static String postAuthor;
+    private static String postUpdated;
+
+    private int defaultImage;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comments);
+
+        setupImageLoader();
+
+        initPost();
+
+    }
+
+    private void initPost() {
+
+        Intent incomingIntent = getIntent();
+        postURL = incomingIntent.getStringExtra("@string/post_url");
+        postThumbnailURL = incomingIntent.getStringExtra("@string/post_thumbnail");
+        postTitle = incomingIntent.getStringExtra("@string/post_title");
+        postAuthor = incomingIntent.getStringExtra("@string/post_author");
+        postUpdated = incomingIntent.getStringExtra("@string/post_updated");
+
+        TextView title = (TextView) findViewById(R.id.postTitle);
+        TextView author = (TextView) findViewById(R.id.postAuthor);
+        TextView updated = (TextView) findViewById(R.id.postUpdated);
+        ImageView thumbnail = (ImageView) findViewById(R.id.postThumbnail);
+        Button btnReply = (Button) findViewById(R.id.btnPostReply);
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.postLoadingProgressBar);
+
+        title.setText(postTitle);
+        author.setText(postAuthor);
+        updated.setText(postUpdated);
+        displayImage(postThumbnailURL, thumbnail, progressBar);
+
+    }
+
+    private void displayImage(String imageURL, ImageView imageView, final ProgressBar progressBar) {
+
+        ImageLoader imageLoader = ImageLoader.getInstance();
+
+        DisplayImageOptions options = new DisplayImageOptions.Builder().cacheInMemory(true)
+                .cacheOnDisc(true).resetViewBeforeLoading(true)
+                .showImageForEmptyUri(defaultImage)
+                .showImageOnFail(defaultImage)
+                .showImageOnLoading(defaultImage).build();
+
+        imageLoader.displayImage(imageURL, imageView, options, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+                progressBar.setVisibility(View.GONE);
+            }
+
+        });
+    }
+
+    private void setupImageLoader() {
+        // UNIVERSAL IMAGE LOADER SETUP
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheOnDisc(true).cacheInMemory(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .displayer(new FadeInBitmapDisplayer(300)).build();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+                CommentsActivity.this)
+                .defaultDisplayImageOptions(defaultOptions)
+                .memoryCache(new WeakMemoryCache())
+                .discCacheSize(100 * 1024 * 1024).build();
+
+        ImageLoader.getInstance().init(config);
+
+        defaultImage = CommentsActivity.this.getResources().getIdentifier("@drawable/reddit_alien", null, CommentsActivity.this.getPackageName());
+    }
+}
