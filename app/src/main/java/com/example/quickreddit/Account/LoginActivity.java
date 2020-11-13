d26703c73e3b74ba881a431781d8813e363735d4
rcorey <metalgearryan@gmail.com>
2020-11-13 15:00:24 -0500

    Started Application Overhaul

--- app/src/main/java/com/example/quickreddit/Account/LoginActivity.java
+++ app/src/main/java/com/example/quickreddit/Account/LoginActivity.java
@@ -28,49 +28,27 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity{
 
     private static final String TAG = "LoginActivity";
+    private URLS urls = new URLS();
 
     private ProgressBar mProgressBar;
     private EditText mUsername;
     private EditText mPassword;
 
-    private URLS urls = new URLS();
-
-    //OAth
-    private static final String AUTH_URL =
-            "https://www.reddit.com/api/v1/authorize.compact?client_id=%s" +
-                    "&response_type=code&state=%s&redirect_uri=%s&" +
-                    "duration=permanent&scope=identity";
-
-    private static final String CLIENT_ID = "ABCDEFGHIJKLM012345-AA";
-
-    private static final String REDIRECT_URI =
-            "http://www.example.com/my_redirect";
-
-    private static final String STATE = "MY_RANDOM_STRING_1";
-
-    private static final String ACCESS_TOKEN_URL =
-            "https://www.reddit.com/api/v1/access_token";
-
-    public void startSignIn(View view) {
-        String url = String.format(AUTH_URL, CLIENT_ID, STATE, REDIRECT_URI);
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-        startActivity(intent);
-    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Log.d(TAG, "onCreate: Started");
-
-        Button btnLogin = (Button) findViewById(R.id.btn_login);
+        Log.d(TAG, "onCreate: started.");
+        final Button btnLogin  = (Button) findViewById(R.id.btn_login);
         mPassword = (EditText) findViewById(R.id.input_password);
         mUsername = (EditText) findViewById(R.id.input_username);
-        mProgressBar = (ProgressBar) findViewById(R.id.LoginRequestLoadingProgressBar);
+        //mProgressBar = (ProgressBar) findViewById(R.id.loginRequestLoadingProgressBar);
         mProgressBar.setVisibility(View.GONE);
+
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,15 +57,17 @@ public class LoginActivity extends AppCompatActivity {
                 String username = mUsername.getText().toString();
                 String password = mPassword.getText().toString();
 
-                if (!username.equals("") && !password.equals("")) {
+                if(!username.equals("") && !password.equals("")){
                     mProgressBar.setVisibility(View.VISIBLE);
+                    //method for signing in
                     login(username, password);
                 }
             }
         });
+
     }
 
-    private void login(final String username, String password) {
+    private void login(final String username, String password){
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(urls.LOGIN_URL)
@@ -99,58 +79,68 @@ public class LoginActivity extends AppCompatActivity {
         HashMap<String, String> headerMap = new HashMap<>();
         headerMap.put("Content-Type", "application/json");
 
-        Call<CheckLogin> call = feedAPI.signIn(headerMap, username, username, password, "json");
+
+
+        Call<CheckLogin> call = feedAPI.signIn(headerMap,username,username, password, "json");
 
         call.enqueue(new Callback<CheckLogin>() {
             @Override
             public void onResponse(Call<CheckLogin> call, Response<CheckLogin> response) {
-                try {
+                try{
+                    //Log.d(TAG, "onResponse: feed: " + response.body().toString());
                     Log.d(TAG, "onResponse: Server Response: " + response.toString());
 
                     String modhash = response.body().getJson().getData().getModhash();
                     String cookie = response.body().getJson().getData().getCookie();
-                    Log.d(TAG, "onResponse: modhash" + modhash);
-                    Log.d(TAG, "onResponse: cookie" + cookie);
+                    Log.d(TAG, "onResponse: modhash: " + modhash);
+                    Log.d(TAG, "onResponse: cookie: " + cookie);
 
-                    if (!modhash.equals("")) {
+                    if(!modhash.equals("")){
                         setSessionParams(username, modhash, cookie);
                         mProgressBar.setVisibility(View.GONE);
                         mUsername.setText("");
                         mPassword.setText("");
                         Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
 
-                        //navigate to previous activity
+                        //navigate back to previous activity
                         finish();
                     }
-                } catch (NullPointerException e) {
-                    Log.e(TAG, "onResponse: NullPointerException: " + e.getMessage());
+                }catch (NullPointerException e){
+                    Log.e(TAG, "onResponse: NullPointerException: " +e.getMessage() );
                 }
             }
 
             @Override
             public void onFailure(Call<CheckLogin> call, Throwable t) {
                 mProgressBar.setVisibility(View.GONE);
-                Log.e(TAG, "onFailure: Unable to retrieve RSS: " + t.getMessage());
+                Log.e(TAG, "onFailure: Unable to retrieve RSS: " + t.getMessage() );
                 Toast.makeText(LoginActivity.this, "An Error Occured", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    private void setSessionParams(String username, String modhash, String cookie) {
+    /**
+     * Save the session params once login in successful
+     * @param username
+     * @param modhash
+     * @param cookie
+     */
+    private void setSessionParams(String username, String modhash, String cookie){
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(LoginActivity.this);
         SharedPreferences.Editor editor = preferences.edit();
 
-        Log.d(TAG, "setSessionParams : Storing session varialbles: \n" +
+        Log.d(TAG, "setSessionParams: Storing session variables:  \n" +
                 "username: " + username + "\n" +
                 "modhash: " + modhash + "\n" +
                 "cookie: " + cookie + "\n"
         );
 
+
         editor.putString("@string/SessionUsername", username);
         editor.commit();
         editor.putString("@string/SessionModhash", modhash);
         editor.commit();
-        editor.putString("@string/SessionCoodie", cookie);
+        editor.putString("@string/SessionCookie", cookie);
         editor.commit();
     }
 }
